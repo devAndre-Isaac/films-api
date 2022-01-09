85e846a5570c92329a72953ac171a50c8fa2ea40
devAndre-Isaac <andreisaac344@gmail.com>
2022-01-09 19:39:26 -0300

    feat: :sparkles: insert AppError in server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,26 @@
 import "reflect-metadata";
-import express from "express";
+import express, { NextFunction, Response, Request } from "express";
+
 import "./database";
 import { routes } from "./routes";
+import AppError from "./errors/AppError";
 
 const app = express();
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: "error",
+        message: error.message,
+      });
+    }
+    return response.status(500).json({
+      status: "error",
+      message: "Internal server Error",
+    });
+  }
+);
 
 app.listen(3000, () => console.log("Server is Running"));
