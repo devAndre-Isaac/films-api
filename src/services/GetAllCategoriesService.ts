2653dae267cb29b93c630128ba13e97dd8979aaa
devAndre-Isaac <andreisaac344@gmail.com>
2022-01-10 18:48:54 -0300

    feat: :sparkles: insert pagination in GetAllCategoriesService

--- src/services/GetAllCategoriesService.ts
+++ src/services/GetAllCategoriesService.ts
@@ -1,12 +1,23 @@
-import { getRepository } from "typeorm";
+import { getCustomRepository } from "typeorm";
 import { Category } from "../entities/Category";
+import CategoriesRepository from "../repositories/CategoriesRepository";
 
+interface IPaginateCategories {
+  from: number;
+  to: number;
+  per_page: number;
+  total: number;
+  current_page: number;
+  prev_page: number | null;
+  next_page: number | null;
+  data: Category[];
+}
 export class GetAllCategoriesService {
-  async execute() {
-    const repository = getRepository(Category);
+  async execute(): Promise<IPaginateCategories> {
+    const repository = getCustomRepository(CategoriesRepository);
 
-    const categories = await repository.find();
+    const categories = await repository.createQueryBuilder().paginate();
 
-    return categories;
+    return categories as IPaginateCategories;
   }
 }
