cb547bc4d46ec2c918aafbf6641ec89717fca697
devAndre-Isaac <andreisaac344@gmail.com>
2022-01-11 17:23:06 -0300

    feat: :sparkles: insert paginate in GetAllVideosService

--- src/services/GetAllVideosService.ts
+++ src/services/GetAllVideosService.ts
@@ -1,14 +1,28 @@
 import { getRepository } from "typeorm";
 import { Video } from "../entities/Video";
+import { getCustomRepository } from "typeorm";
+import VideosRepository from "../repositories/VideosRepository";
 
+interface IPaginateVideos {
+  from: number;
+  to: number;
+  per_page: number;
+  total: number;
+  current_page: number;
+  prev_page: number | null;
+  next_page: number | null;
+  data: Video[];
+}
 export class GetAllVideosService {
-  async execute() {
-    const repository = getRepository(Video);
+  async execute(): Promise<IPaginateVideos> {
+    const repository = getCustomRepository(VideosRepository);
 
-    const videos = await repository.find({
-      relations: ["category"],
-    });
+    const videos = await repository
+      .createQueryBuilder({
+        relations: ["category"],
+      } as any)
+      .paginate();
 
-    return videos;
+    return videos as IPaginateVideos;
   }
 }
