6031836b70c323e1987585d769caa1f4e89151c0
devAndre-Isaac <andreisaac344@gmail.com>
2022-01-12 18:33:07 -0300

    refactor: :recycle: change GetAllVideosService

--- src/services/GetAllVideosService.ts
+++ src/services/GetAllVideosService.ts
@@ -1,17 +1,8 @@
 import { Video } from "../entities/Video";
 import { getCustomRepository } from "typeorm";
 import VideosRepository from "../repositories/VideosRepository";
+import { IPaginateVideos } from "../interfaces/paginate";
 
-interface IPaginateVideos {
-  from: number;
-  to: number;
-  per_page: number;
-  total: number;
-  current_page: number;
-  prev_page: number | null;
-  next_page: number | null;
-  data: Video[];
-}
 export class GetAllVideosService {
   async execute(): Promise<IPaginateVideos> {
     const repository = getCustomRepository(VideosRepository);
