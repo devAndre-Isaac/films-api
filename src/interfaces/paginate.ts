2ccd0b904bcfcc753ac9c50b3b4f96635796fd61
devAndre-Isaac <andreisaac344@gmail.com>
2022-01-12 18:32:42 -0300

    feat: :sparkles: add paginate interfaces

---
+++ src/interfaces/paginate.ts
@@ -0,0 +1,24 @@
+import { Category } from "../entities/Category";
+import { Video } from "../entities/Video";
+
+export interface IPaginateVideos {
+  from: number;
+  to: number;
+  per_page: number;
+  total: number;
+  current_page: number;
+  prev_page: number | null;
+  next_page: number | null;
+  data: Video[];
+}
+
+export interface IPaginateCategories {
+  from: number;
+  to: number;
+  per_page: number;
+  total: number;
+  current_page: number;
+  prev_page: number | null;
+  next_page: number | null;
+  data: Category[];
+}
